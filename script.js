cc5a7eab8ba1c95a630a4f77ef9f03040e90efa3
sparsh-gupt <72927084+sparsh-gupt@users.noreply.github.com>
2020-10-30 18:42:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function show(){
+    document.getElementById("in1").style.visibility="visible";
+    window.alert("MY PARENTS ARE MY INSPIRATION THEY GAVE ME ALL THE THINGS AND GAVE ALL NICE BEHAVOIRS.");
+} 
